063c17547be3f9f715d2bdef13f1840ab1faa043
varaprasad <varaprasad.v@nybinfotech.com>
2025-05-29 13:15:25 +0530

    Update package.json with production

--- client/vite.config.js
+++ client/vite.config.js
@@ -6,10 +6,24 @@
 //   plugins: [react()],
 // })
 
+// import { defineConfig } from 'vite';
+// import react from '@vitejs/plugin-react';
+// import tailwindcss from '@tailwindcss/vite';
+// // https://vite.dev/config/
+// export default defineConfig({
+//   plugins: [react(), tailwindcss()],
+// });
+
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import tailwindcss from '@tailwindcss/vite';
+
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  server: {
+    
+    port: import.meta.env.VITE_PORT || 5173,
+    host: '0.0.0.0', 
+  },
 });
